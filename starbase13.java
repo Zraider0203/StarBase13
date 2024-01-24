6a4920dbbf54d48c4ef0dae6cf73a38d69bd7c48
Zraider0203 <144168449+Zraider0203@users.noreply.github.com>
2024-01-24 16:36:28 -0700

    Add files via upload
    First Upload

---
+++ starbase13.java
@@ -0,0 +1,17 @@
+// Import the scanner class which will allow us to get input from user
+import java.util.Scanner;
+
+// Create a class called starbase13, note that the class name must match the file name
+public class starbase13{
+    // Create our main method, this is where the program will start alway!
+    public static void mai(string[] args){
+         // Create a welcome output to our user
+        system.out.println("welcome to Starbase13, the luckiest starbase in the galaxy!");
+        // Great the user and ask for their name
+        System.out.println(x:"what is your name?");
+        // Create a string variable to hold the user's name
+        String userName = "";
+        // Create a scanner object to get input from the user
+    }
+   
+}
